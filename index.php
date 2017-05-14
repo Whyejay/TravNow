0af2b63657f8ccb429f8d9762bbaff82a7df1f6c
Tan Yi Jie <yijie.tan@uq.net.au>
2017-05-14 23:05:30 +1000

    added 5 trending places. [need to edit the text]

--- index.php
+++ index.php
@@ -39,19 +39,67 @@ require_once 'php/check_logging.php';
     <hr></hr>
     <div class="trend text-center img-responsive container-fluid col-sm-6">
     </div>
-    <div class="trendtext text-center container-fluid col-sm-4">
-    <p>The Sydney Harbour Bridge, also affectionately known as the 'Coathanger', was opened on March 19th 1932 by Premier Jack Lang, after six years of construction. Made of steel the bridge contains 6 million hand driven rivets. The surface area that requires painting is equal to about the surface area of 60 sports fields. The Bridge has huge hinges to absorb the expansion caused by the hot Sydney sun. You will see them on either side of the bridge at the footings of the Pylons.</p>
+    <div class="trendtext col-sm-4">
+    <h2>Sydney Harbour Bridge</h2>
+    <small>The Sydney Harbour Bridge, also affectionately known as the 'Coathanger', was opened on March 19th 1932 by Premier Jack Lang, after six years of construction. Made of steel the bridge contains 6 million hand driven rivets. The surface area that requires painting is equal to about the surface area of 60 sports fields. The Bridge has huge hinges to absorb the expansion caused by the hot Sydney sun. You will see them on either side of the bridge at the footings of the Pylons.
+
+    The views and photo opportunities are fantastic. (If you can make it, we've got to say it is tough). There is a great display on how the thing was built. It has a similar place in Sydney history to the Statue of Liberty in New York as far as many migrants to Australia go. In sight of the bridge you knew you had made it.
+
+    The displaced peoples of Europe who came to Australia in the days of the grand ships can get very misty when you ask them what they felt when they saw this grand old arch on their arrival in Sydney from the aftermath of World War Two as they sailed up Port Jackson (Sydney Harbour). The old Bridge has been replaced as "the" landmark of Sydney by the bold architecture of the Opera House.</small>
+
+    <button id="btnReadmore" type="submit" class="btn btn-primary">Read More</button>
     </div>
     <div class="trend2 text-center img-responsive container-fluid col-sm-6">
     </div>
+    <div class="trendtext col-sm-4">
+    <h2>The Kootenay Rockies</h2>
+    <small>The Rockies evoke images of towering, snow-capped peaks, turquoise lakes, cascading waterfalls and dense evergreen forests. In BC, that jaw-dropping, Rocky Mountain scenery – the kind that stays with you long after you’ve returned home – stretches the entire length of the province. UNESCO established the Canadian Rocky Mountain Parks World Heritage Site along the BC/Alberta border, in part because of this spectacular beauty. The area is also globally significant because of the Burgess Shale fossil site, which shows in amazing detail what the Earth was like more than half a billion years ago.
+
+    Four of British Columbia's seven national parks are located here, and wildlife flourishes. During the winter, the region is home to some of the continent's finest powder.
+
+    Outdoor pursuits here are epic. The area is one of Canada's pre-eminent destinations for hiking and mountain biking, as well as for whitewater rafting. Golf courses are the perfect combination of challenging and stunningly beautiful, and the region is internationally renowned for its abundant fishing locales.
+
+    There are also opportunities to visit wonderfully restored heritage towns, thriving arts communities and gold rush boomtowns. Accommodations range from wilderness camping to luxurious stays in a natural setting.
+    </small>
+    <button id="btnReadmore" type="submit" class="btn btn-primary">Read More</button>
+
+    </div>
     <div class="trend3 text-center img-responsive container-fluid col-sm-6">
+    </div>
+    <div class="trendtext col-sm-4">
+    <h2>Sveti Stefan</h2>
+    <small>Sveti Stefan (Montenegrin pronunciation: [sv̞ê̞ːtiː stê̞faːn], lit. "Saint Stephen"; Cyrillic: Свети Стефан, Italian: Santo Stefano) is a small islet and 5-star hotel resort on the Adriatic coast of Montenegro, approximately 6 kilometres (3.7 mi) southeast of Budva.
+
+    The resort, known commercially as Aman Sveti Stefan, includes part of the mainland, where the Villa Miločer (Montenegrin: [vîla mîlɔ̝tʃe̞r]) part of the resort is located. An Adriatic playground for the rich and famous from the 1960s to the 1980s, the hotel is now a 5-star franchise hotel of the international group of Aman Resorts, completed in 2009 and operating under a 30-year lease. Formerly an island, Sveti Stefan is now connected to the mainland by a narrow isthmus. The resort in total contains 50 rooms, cottages and suites on the island and 8 grand suites at the Villa Miločer.
+    </small>
+    <button id="btnReadmore" type="submit" class="btn btn-primary">Read More</button>
     </div>
     <div class="trend4 text-center img-responsive container-fluid col-sm-6">
     </div>
+    <div class="trendtext col-sm-4">
+    <h2>Alaska Highway</h2>
+    <small>The Alaska Highway (also known as the Alaskan Highway, Alaska-Canadian Highway, or ALCAN Highway) was constructed during World War II for the purpose of connecting the contiguous United States to Alaska across Canada. It begins at the junction with several Canadian highways in Dawson Creek, British Columbia, and runs to Delta Junction, Alaska, via Whitehorse, Yukon. Completed in 1942 at a length of approximately 1,700 miles (2,700 km), as of 2012 it is 1,387 mi (2,232 km) long. The difference in distance is due to constant reconstruction of the highway, which has rerouted and straightened out numerous sections. The highway was opened to the public in 1948.[1] Legendary over many decades for being a rough, challenging drive, the highway is now paved over its entire length.
+
+    An informal system of historic mileposts developed over the years to denote major stopping points; Delta Junction, at the end of the highway, makes reference to its location at "Historic Milepost 1422." It is at this point that the Alaska Highway meets the Richardson Highway, which continues 96 mi (155 km) to the city of Fairbanks. This is often regarded, though unofficially, as the northern portion of the Alaska Highway, with Fairbanks at Historic Milepost 1520.[2] Mileposts on this stretch of highway are measured from Valdez, rather than the Alaska Highway. The Alaska Highway is popularly (but unofficially) considered part of the Pan-American Highway, which extends south (despite its discontinuity in Panama) to Argentina.
+    </small>
+    <button id="btnReadmore" type="submit" class="btn btn-primary">Read More</button>
+    </div>
     <div class="trend5 text-center img-responsive container-fluid col-sm-6">
     </div>
-</div>
+    <div class="trendtext col-sm-4">
+    <h2>Okanagan Valley</h2>
+    <small>
+    The natural landscape of the Thompson Okanagan includes mountains, valleys, desert, and everything in between.
 
+    The highest mountain in the Canadian Rockies is here, as is a waterfall twice the height of Niagara Falls, and Canada's only true desert environment.
+
+    This is a region that truly has something for everyone. Foodies flock to the area's renowned orchards and vineyards; families can't get enough of the many sandy, lakeside beaches; and outdoor enthusiasts come for the golfing, hiking, biking and skiing.
+
+    The history and culture of the region - from its Aboriginal peoples to European fur traders to wine makers and food producers - is strongly tied to the land. This is reflected in many of the area's museums, heritage sites, and much of its artwork, where the colourful past is brought to life for visitors.
+    </small>
+    <button id="btnReadmore" type="submit" class="btn btn-primary">Read More</button>
+    </div>
+    </div>
 ​    <br>
     <br>
     <br>
