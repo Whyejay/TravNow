0d0240ec6e48bb1476370bd397e4e9efd3b43ecb
s4421554 <duonganhkhoa95@gmail.com>
2017-05-17 16:11:37 +1000

    load data on scrolling

--- php/article.php
+++ php/article.php
@@ -1,29 +1,58 @@
 <?php
 include_once 'Parsedown.php';
-while ($post = $article_list->fetch_assoc()) {
-    $Parsedown = new Parsedown();
+$Parsedown = new Parsedown();
+
+// connect database
+require_once 'connectMySQL.php';
+$database = new MySQLDatabase();
+$db_connection = $database->connect();
+
+// process
+$last_id = $_REQUEST['last_id'];
+$selectQuery = "SELECT * FROM post WHERE post_id > '$last_id' ORDER BY post_id ASC LIMIT 2";
+$select = $db_connection->query($selectQuery);
+while ($post = $select->fetch_assoc()) {
     $content = $Parsedown->text($post['content']);
+    $post_id = $post['post_id'];
     $title = $post['title'];
     $username = $post['user_username'];
+    preg_match('/<img.+src=[\'"](?P<src>.+?)[\'"].*>/i', $content, $image);
+    $first_image_src = $image['src'];
+    $text_without_image = preg_replace("/<img[^>]+\>/i", "", $content);
+    $description = tease($text_without_image);
     ?>
-    <html>
-    <head>
-    </head>
-    <body>
-    <div class="row">
-        <div class="col-sm-4"><a href="#" class=""><img src="img/reyes1.jpg" class="img-responsive"></a>
+    <div class="row" id="<?php echo $post_id ?>">
+        <div class="col-sm-4"><a href="#" class=""><img src="<?php echo $first_image_src ?>" class="img-responsive"></a>
         </div>
         <div class="col-sm-8">
-            <h3 class="title">How to Fight Fraud with Artificial Intelligence and Intelligent Analytics</h3>
+            <h3 class="title"><?php echo $title ?></h3>
             <p class="text-muted"><span class="glyphicon glyphicon-lock"></span> Available Exclusively for Premium
                 Members</p>
-            <p>Could artificial intelligence have been used to prevent the high-profile Target breach? The concept is
-                not so far-fetched. Organizations such as Mastercard and RBS WorldPay have long relied on artificial
-                intelligence to detect fraudulent transaction patterns and prevent card.</p>
-
-            <p class="text-muted">Presented by <a href="#">Ellen Richey</a></p>
+            <p><?php echo $description ?></p>
+            <p class="text-muted">Presented by <a href="#"><?php echo $username ?></a></p>
+            <button onclick="window.location.href='https://infs3202-c25wl.uqcloud.net/travnow/content.php?id=<?php echo $post_id ?>'" id="btnReadmore" type="submit" class="btn btn-primary">Read More</button>
         </div>
     </div>
-    </body>
-    </html>
-<?php } ?>
+    <hr>
+    <?php
+}
+$db_connection->close();
+function tease($body, $sentencesToDisplay = 4) {
+    $nakedBody = preg_replace('/\s+/',' ',strip_tags($body));
+    $sentences = preg_split('/(\.|\?|\!)(\s)/',$nakedBody);
+
+    if (count($sentences) <= $sentencesToDisplay)
+        return $nakedBody;
+
+    $stopAt = 0;
+    foreach ($sentences as $i => $sentence) {
+        $stopAt += strlen($sentence);
+
+        if ($i >= $sentencesToDisplay - 1)
+            break;
+    }
+
+    $stopAt += ($sentencesToDisplay * 2);
+    return trim(substr($nakedBody, 0, $stopAt));
+}
+?>
