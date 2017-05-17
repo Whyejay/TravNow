18ef716e705d4ff82743b07033794f419a918ea1
s4421554 <duonganhkhoa95@gmail.com>
2017-05-18 00:57:34 +1000

    unset session, cookie when logout

--- php/logout.php
+++ php/logout.php
@@ -7,9 +7,10 @@ session_start();
 session_unset();
 session_destroy();
 if(isset($_COOKIE[session_name()])) {
-    setcookie(session_name(), '', time() - 3600);
+    setcookie(session_name(), '', 1);
 }
 if(isset($_COOKIE['remember_me'])) {
-    unset($_COOKIE['remember_me']);
-    setcookie('remember_me', '', time() - 3600);
+    $domain = $_SERVER['HTTP_HOST'];
+    setcookie('remember_me', '', 1, '/', $domain);
 }
+header('Location: https://infs3202-c25wl.uqcloud.net/travnow/');
